f0006d415fe06fe143f7b7e115c2fff45868f282
SrHell <israellalves1986@gmail.com>
2022-06-04 03:07:32 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    botClientID: "ID DO BOT", // COMO PEGA O CLIENT ID (https://discord.com/developers/applications/ID-BOT/oauth2/general
+    botPrefix: "!",
+    ownerID: "ID DO ADMIN",
+    embedColor: "#00FF00",
+}
